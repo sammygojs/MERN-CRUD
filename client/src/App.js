4ffbcab8195ab5bbb340d5c17d558c141c134052
Sumit <akoliyasumit@hotmail.com>
2022-02-08 23:22:21 +0530

    Post data from form to server and then mongo

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,27 @@
+import { useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+  const [foodName, setFoodName] = useState('')
+  const [days, setDays] = useState(0)
+
+  const submitFood = () =>{
+    Axios.post('http://localhost:3001/api/insert',{foodName:foodName,days:days}).then((response)=>alert(`row inserted ${response.data}`))
+    const name = foodName;
+    const day = days;
+    console.log("day: ",day)
+    console.log("name: ",name)
+  }
+
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <h1>Hello CRUD APP</h1>
+      <label>FoodName</label>
+      <input onChange={(e)=>{setFoodName(e.target.value)}} type="text" ></input>
+      <label>Days since you ate it</label>
+      <input onChange={(e)=>{setDays(e.target.value)}} type="number"></input>
+      <button onClick={submitFood}>Add to list</button>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const FoodModel = require('./models/Food');
-
+const cors = require('cors')
+app.use(cors())
 app.use(express.json())
 
 mongoose.connect(
@@ -21,6 +22,23 @@ db.once("open", function () {
 app.get('/', async (req,res)=>{
     const Food = new FoodModel({foodName:"Apple",daysSinceAte:2})
     try{
+        await Food.save()
+        res.send("data inserted")
+    }
+    catch (err){
+        console.log("error: ",err)
+    }
+})
+
+app.post('/api/insert', async (req,res)=>{
+    // const name = req.body.foodName;
+    // const day = req.body.days;
+    const Food = new FoodModel({foodName:req.body.foodName,daysSinceAte:req.body.days})
+    // console.log("day: ",day)
+    // console.log("name: ",name)
+    try{
+        console.log(Food)
+        // res.send(`hi`)
         await Food.save()
         res.send("data inserted")
     }
